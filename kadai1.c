657c004eed5606c742a0f2eacd874b91f62e28bb
towa-s1027 <148538386+towa-s1027@users.noreply.github.com>
2023-10-27 19:16:05 +0900

    Add files via upload

---
+++ kadai1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, result;
+
+	//®”‚ğ“ü—Í
+	printf("Please Enter Number: ");
+	scanf_s("%d", &a);
+
+	//‚Qæ‚ğŒvZ
+	result = a * a;
+
+	printf(" %d * %d =%d", a,a, result);
+
+	return 0;
+}
+
