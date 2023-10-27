ecf9184696e10e180f393e3821e11fbfc10cd0a3
towa-s1027 <148538386+towa-s1027@users.noreply.github.com>
2023-10-27 19:16:47 +0900

    Add files via upload

---
+++ kadai2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a, result;
+
+	//®”‚ğ“ü—Í
+	printf("Please Enter Number: ");
+	scanf_s("%d", &a);
+
+	if (a % 2 == 1)
+	{
+		printf("%d‚ÍŠï”",a);
+	}
+	else 
+	{
+		printf("%d‚Í‹ô”",a);
+	}
+
+	return 0;
+}
+
+	
